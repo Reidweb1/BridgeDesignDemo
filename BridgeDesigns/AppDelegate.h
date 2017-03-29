8b6f9c3d538736eaffcccb5d5f19f3afd4b897c1
Reid Weber <reid@meshstudio.io>
2017-03-29 08:51:26 -0700

    Initial Commit

---
+++ BridgeDesigns/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BridgeDesigns
+//
+//  Created by Reid Weber on 3/29/17.
+//  Copyright © 2017 Reid Weber. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BridgeDesigns/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BridgeDesigns
+//
+//  Created by Reid Weber on 3/29/17.
+//  Copyright © 2017 Reid Weber. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
